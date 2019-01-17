a6d15b0ee734d524404247404fb074ecb57ea7dd
OnlineGardenGnome <raphael.j.laporte@gmail.com>
2019-01-17 15:26:10 +0100

    What I've been working on :)

---
+++ raphael_temp/script_config.py
@@ -0,0 +1,7 @@
+_config_hops                               = 2
+_config_data_folder                        = "/nvme/drive_1/NTDS_Final/"
+_config_relation_list_size_neighborhood    = 10000000
+_config_maximum_neighborhood_size          = 66241
+_config_random_seed                        = 1
+_config_availible_threads                  = 12
+_config_feature_permutations               = 10       
